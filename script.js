d8e462441b77e634f151a7b01a46a78fab71710c
Liao23 <81964976+Justin-Liao23-e@users.noreply.github.com>
2024-05-22 01:27:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-// Function to toggle the display of social media links
 function toggleSocial(personId) {
     var socialDiv = document.getElementById(personId);
     if (socialDiv.style.display === "block") {
-        socialDiv.style.display = "none";  // Hide the icons if visible
+        socialDiv.style.display = "none";
     } else {
-        socialDiv.style.display = "block"; // Show the icons if hidden
+        socialDiv.style.display = "block";
+        socialDiv.style.transition = "all 0.3s ease";
+        socialDiv.style.transform = "translateY(0%)";
     }
 }
 
-// Function to close social media links if the user clicks outside of them
 window.onclick = function(event) {
-    // Retrieve all elements with class 'social'
     var socials = document.getElementsByClassName('social');
     for (var i = 0; i < socials.length; i++) {
         var openSocial = socials[i];
-        // Check if the click is outside of a social div and it is open
         if (event.target.tagName !== 'IMG' && openSocial.style.display === 'block') {
-            openSocial.style.display = 'none'; // Hide the currently displayed icons
+            openSocial.style.display = 'none';
         }
     }
 }
