1c3996e4d29e8683f35b23a6ef64c24291c0d84f
Liao23 <81964976+Justin-Liao23-e@users.noreply.github.com>
2024-05-22 01:04:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ function showDetails(name, title, socialInfo) {
     document.getElementById('modalTitle').textContent = title;
     document.getElementById('modalSocial').textContent = socialInfo;
 
-    modal.style.display = 'block';
+    modal.style.display = 'block'; // Show the modal
 }
 
 function closeModal() {
-    document.getElementById('infoModal').style.display = 'none';
+    document.getElementById('infoModal').style.display = 'none'; // Hide the modal
 }
 
 window.onclick = function(event) {
     const modal = document.getElementById('infoModal');
     if (event.target == modal) {
-        modal.style.display = 'none';
+        modal.style.display = 'none'; // Hide the modal when clicking outside
     }
 }
