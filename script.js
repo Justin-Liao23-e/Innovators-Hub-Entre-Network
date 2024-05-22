c8f928b8dc729d5563ab9ab7790e6365396cc28e
Liao23 <81964976+Justin-Liao23-e@users.noreply.github.com>
2024-05-22 02:38:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-function toggleDropdown() {
-    var dropdown = document.getElementById("dropdown-menu");
-    if (dropdown.style.display === "block") {
-        dropdown.style.display = "none";
-    } else {
-        dropdown.style.display = "block";
-    }
-}
+
