5d9c498b0938eb88da7d14228597af881c0f1f84
Liao23 <81964976+Justin-Liao23-e@users.noreply.github.com>
2024-05-22 23:05:58 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+// Toggle dropdown function
+document.querySelector('.dropbtn').addEventListener('click', function() {
+    const dropdownContent = document.querySelector('.dropdown-content');
+    if (dropdownContent.style.display === 'block') {
+        dropdownContent.style.display = 'none';
+        dropdownContent.style.maxHeight = '0';
+    } else {
+        dropdownContent.style.display = 'block';
+        dropdownContent.style.maxHeight = '500px';
+    }
+});
